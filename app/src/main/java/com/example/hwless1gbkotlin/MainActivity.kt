1478d09534a0fba779bf0492f7d885c8b5c40cc7
mys_ya_ <7nacu7@gmail.com>
2021-08-31 13:37:46 +0300

    b. Создать Object. В Object вызвать copy и вывести значения скопированного класса на экран. c. Вывести значения из разных циклов в консоль, используя примеры из методических материалов.

--- app/src/main/java/com/example/hwless1gbkotlin/MainActivity.kt
+++ app/src/main/java/com/example/hwless1gbkotlin/MainActivity.kt
@@ -2,16 +2,23 @@ package com.example.hwless1gbkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        private const val TAG_HW_EX_C = "HW_EX_C"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         buttonReaction()
+
+        exC()
     }
 
     private fun buttonReaction() {
@@ -19,10 +26,38 @@ class MainActivity : AppCompatActivity() {
         val buttonExA = findViewById<Button>(R.id.buttonA)
         val textView = findViewById<TextView>(R.id.text_view_person)
 
+        val textViewCopy = findViewById<TextView>(R.id.text_view_person_copy)
+        val buttonExB = findViewById<Button>(R.id.buttonB)
+
         buttonExA.setOnClickListener() {
             textView.text = stringTextView(person)
         }
+
+        buttonExB.setOnClickListener() {
+            val personCopy = person.copy()
+            textViewCopy.text = stringTextView(personCopy)
+        }
+
     }
 
+    private fun exC() {
+        Log.d(TAG_HW_EX_C, "Cycle for (i in 1..25) ")
+        for (i in 1..25) {
+            Log.d(TAG_HW_EX_C, i.toString())
+        }
 
+        Log.d(TAG_HW_EX_C, "Cycle for (1 until 25) ")
+        for (i in 1 until 10) {
+            Log.d(TAG_HW_EX_C, i.toString())
+        }
+
+        Log.d(TAG_HW_EX_C, "Cycle for (i in 25 downTo 1 step 5) ")
+        for (i in 10 downTo 1 step 5) {
+            Log.d(TAG_HW_EX_C, i.toString())
+        }
+
+        Log.d(TAG_HW_EX_C, "List, size=10 => .forEach { } ")
+        val testArray = List(10) { it }
+        testArray.forEach { Log.d(TAG_HW_EX_C, it.toString()) }
+    }
 }
